d3a362619003a86ac858f7df6b50cffdf6d8d186
Sajal Chourasia <sajalchourasia19@gmail.com>
2026-08-14 21:49:00 +0530

    Time: 0 ms (100%), Space: 8.3 MB (51.34%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isHappy(int n)
+    {
+        unordered_set<int> s;
+        int a=0;
+        while(n!=0 && s.find(n)==s.end())
+        {
+            s.insert(n);
+            int b=0;
+            while(n!=0)
+            {
+                a=n%10;
+                n/=10;
+                b+=a*a;
+            }
+            n=b;
+            if (n==1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
