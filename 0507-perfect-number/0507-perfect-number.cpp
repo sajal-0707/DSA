6581db3fe4512ec2eb6aac7af74af9290f883629
Sajal Chourasia <sajalchourasia19@gmail.com>
2026-08-22 23:03:52 +0530

    Time: 0 ms (100%), Space: 7.8 MB (21.28%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -3,9 +3,7 @@ public:
     bool checkPerfectNumber(int num)
     {
         if(num <= 1) return false;
-
         int temp = 1;
-
         for(int i = 2; (long long)i * i <= num; i++)
         {
             if(num % i == 0)
@@ -17,7 +15,6 @@ public:
                 }
             }
         }
-
         return temp == num;
     }
 };
