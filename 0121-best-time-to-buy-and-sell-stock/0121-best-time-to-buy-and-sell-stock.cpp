cffa28d4dd3e055e0312bf305f52841529cdc23e
Sajal Chourasia <sajalchourasia19@gmail.com>
2026-09-20 23:25:29 +0530

    Time: 0 ms (100%), Space: 97.3 MB (61.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
     {
         int min = prices[0];
         int max = 0;
-        for (int i = 0; i < prices.size(); i++) {
+        for ( int i = 0 ; i < prices.size() ; i++ ) {
             if (prices[i] < min) {
                 min = prices[i];
             } else if (prices[i] - min > max) {
