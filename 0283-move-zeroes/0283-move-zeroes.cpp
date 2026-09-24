8534c8195dd2497627b3cba0ac87c5a9a7656869
Sajal Chourasia <sajalchourasia19@gmail.com>
2026-09-24 09:31:11 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.5%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size(), j = 0;
+        int n = nums.size(), j = 0, temp=0;
         for (int i = 0; i < n; i++) {
             if (nums[i] != 0) {
-                swap(nums[i], nums[j]);
+                // swap(nums[i], nums[j]);
+                temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
                 j++;
             }
         }
